0174547916526e4569ffa5a297f5585025189808
Dipen100 <chhetridipen10@gmail.com>
2024-09-19 13:50:47 +0545

    images

--- recipe/forms.py
+++ recipe/forms.py
@@ -18,7 +18,11 @@ class RecipeForm(forms.ModelForm):
         }
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(
+        max_length=30, 
+        required=True, 
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
     password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 class SignupForm(UserCreationForm):
